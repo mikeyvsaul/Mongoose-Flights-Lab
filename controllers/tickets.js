c5ce7daadef486f71dae8c367d34c42190f69ff6
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-08-31 08:12:40 -0700

    Functionality complete

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,9 +1,10 @@
-const Flight = require('../models/flight');
 const Ticket = require('../models/ticket');
+const Flight = require('../models/flight');
 
 module.exports = {
   new: newTicket,
-  create
+  create,
+  delete: deleteTicket
 }
 
 function newTicket(req, res) {
@@ -14,5 +15,11 @@ function create(req, res) {
   req.body.flight = req.params.id
   Ticket.create(req.body, function(err, tickets) {
     res.redirect(`/flights/${req.params.id}`)
+  })
+}
+
+function deleteTicket(req, res) {
+  Ticket.findByIdAndRemove(req.params.id, function(err, ticket) {
+    res.redirect(`/flights/${ticket.flight}`);
   })
 }
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const ticketsCtrl = require('../controllers/tickets')
+const ticketsCtrl = require('../controllers/tickets');
+const ticket = require('../models/ticket');
 
 router.get('/flights/:id/tickets/new', ticketsCtrl.new);
 router.post('/flights/:id/tickets', ticketsCtrl.create);
+router.delete('/tickets/:id', ticketsCtrl.delete);
 
 module.exports = router;
